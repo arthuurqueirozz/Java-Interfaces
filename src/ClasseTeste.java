b5fa88021e24b747a09b5819a4db4c1effd43ae1
Arthur Queiroz <arthurqueiroz25@gmail.com>
2024-09-04 22:54:26 -0300

    creating implementation of interface in classes

--- src/ClasseTeste.java
+++ src/ClasseTeste.java
@@ -1,12 +1,24 @@
 import interfaces.CanetaEsferografica;
+import interfaces.Giz;
 import interfaces.ICaneta;
+import interfaces.Lapis;
 
 public class ClasseTeste {
 
-    public static void main (String args[]) {
+    public static void main (String[] args) {
 
         ICaneta caneta = new CanetaEsferografica();
         caneta.escrever("hello, world!");
         System.out.println(caneta.getCor());
+
+        ICaneta giz = new Giz();
+        giz.escrever("hello, world!");
+        giz.escreverComumAtodas();
+        System.out.println(giz.getCor());
+
+        ICaneta lapis = new Lapis();
+        lapis.escrever("hello, world!");
+        lapis.escreverComumAtodas();
+        System.out.println(lapis.getCor());
     }
 }
--- src/interfaces/ICaneta.java
+++ src/interfaces/ICaneta.java
@@ -4,4 +4,8 @@ public interface ICaneta {
     public void escrever(String texto);
 
     public String getCor ();
+
+    default void escreverComumAtodas() {
+        System.out.println("Escrita igual a todas.");
+    }
 }
